89d9dadb8341cc9011fba6094ef25d7503324636
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-04 17:45:16 +0100

    main css adjustments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,24 +8,44 @@ export default function Main() {
         <div className="container text-center">
           <div className="row row-cols-5">
             <div className="col">
-              <img src="../../public/buy-comics-digital-comics.png" alt="" />
-              item
+              <img
+                src="../../public/buy-comics-digital-comics.png"
+                alt=""
+                className="mx-2"
+              />
+              DIGITAL COMICS
             </div>
             <div className="col">
-              <img src="../../public/buy-comics-merchandise.png" alt="" />
-              item
+              <img
+                src="../../public/buy-comics-merchandise.png"
+                alt=""
+                className="mx-2 t-shirt"
+              />
+              DC MERCHANDISE
             </div>
             <div className="col">
-              <img src="../../public/buy-comics-subscriptions.png" alt="" />
-              item
+              <img
+                src="../../public/buy-comics-subscriptions.png"
+                alt=""
+                className="mx-2"
+              />
+              SUBSCRIPTION
             </div>
             <div className="col">
-              <img src="../../public/buy-comics-shop-locator.png" alt="" />
-              item
+              <img
+                src="../../public/buy-comics-shop-locator.png"
+                alt=""
+                className="mx-2 shop-location"
+              />
+              COMIC SHOP LOCATOR
             </div>
             <div className="col">
-              <img src="../../public/buy-dc-power-visa.svg" alt="" />
-              item
+              <img
+                src="../../public/buy-dc-power-visa.svg"
+                alt=""
+                className="mx-2"
+              />
+              DC POWER VISA
             </div>
           </div>
         </div>
