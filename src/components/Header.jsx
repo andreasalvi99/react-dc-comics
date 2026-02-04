4a8b935dec78c8f02f80449eff607a7c3b24b688
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-04 15:33:43 +0100

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,23 @@
 export default function Header() {
-  return <header>Ciaooooo</header>;
+  return (
+    <header>
+      <div id="dc-logo" className="py-3">
+        <img src="../../public/dc-logo.png" alt="dc-logo" />
+      </div>
+      <div id="navbar" className="py-3">
+        <ul>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+          <li className="py-3">item</li>
+        </ul>
+      </div>
+    </header>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./assets/css/index.css";
-import App from "./App.jsx";
 
 //?? Import Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
+
+// ?? CSS
+import "./assets/css/index.css";
+
+// ?? Components
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
