1e16ad2954e43fe2fbef1b4634b4ebf07e4cf4ba
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-04 15:00:13 +0100

    added bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
 import App from "./App.jsx";
 
+//?? Import Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as bootstrap from "bootstrap";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
