fe0cfe9d83961237578d99f35544d1815075a90f
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-04 18:08:28 +0100

    navbar css animations dpne

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,18 @@ export default function Header() {
       <div id="dc-logo" className="py-3">
         <img src="../../public/dc-logo.png" alt="dc-logo" />
       </div>
-      <div id="navbar" className="py-3">
+      <div id="navbar" className="">
         <ul>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
-          <li className="py-3">item</li>
+          <li className="fw-bold">CHARACTERS</li>
+          <li className="fw-bold">COMICS</li>
+          <li className="fw-bold">MOVIES</li>
+          <li className="fw-bold">TV</li>
+          <li className="fw-bold">SHOP</li>
+          <li className="fw-bold">NEWS</li>
+          <li className="fw-bold">FANS</li>
+          <li className="fw-bold">VIDEOS</li>
+          <li className="fw-bold">COLLECTIBLES</li>
+          <li className="fw-bold">GAMES</li>
         </ul>
       </div>
     </header>
