c6b3abad004cb58ddaf623b8cf12ca9b1d8038ba
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-04 16:33:33 +0100

    footer progress

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,30 @@
 export default function Footer() {
-  return <footer>Ciaooooo</footer>;
+  return (
+    <footer>
+      <section id="about-us">
+        <div className="container text-center">
+          <div className="links"></div>
+          <div className="bg-img">
+            <img src="../../public/dc-logo-bg.png" alt="" />
+          </div>
+        </div>
+      </section>
+      <section id="socials" className="bg-dark">
+        <div className="container text-center">
+          <div id="register-btn">
+            <button></button>
+          </div>
+          <div id="social-media">
+            <ul>
+              <li>item</li>
+              <li>item</li>
+              <li>item</li>
+              <li>item</li>
+              <li>item</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
 }
