750948b64b21d8694877459d02af5931926b5fcc
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-04 15:51:50 +0100

    main sections done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,35 @@
 export default function Main() {
-  return <main>Ciaooooo</main>;
+  return (
+    <main>
+      <section id="contents" className="bg-black">
+        <div className="container text-start"> ---Content goes here--- </div>
+      </section>
+      <section id="adds-on" className="bg-primary">
+        <div className="container text-center">
+          <div className="row row-cols-5">
+            <div className="col">
+              <img src="../../public/buy-comics-digital-comics.png" alt="" />
+              item
+            </div>
+            <div className="col">
+              <img src="../../public/buy-comics-merchandise.png" alt="" />
+              item
+            </div>
+            <div className="col">
+              <img src="../../public/buy-comics-subscriptions.png" alt="" />
+              item
+            </div>
+            <div className="col">
+              <img src="../../public/buy-comics-shop-locator.png" alt="" />
+              item
+            </div>
+            <div className="col">
+              <img src="../../public/buy-dc-power-visa.svg" alt="" />
+              item
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
