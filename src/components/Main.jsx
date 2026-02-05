7f59e73913bd35d1a5af7755598cb191013a85c6
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-05 13:24:55 +0100

    jumbotron placed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ export default function Main() {
   return (
     <main>
       <section id="contents" className="bg-black">
+        <div id="jumbotron"></div>
         <div className="container text-start">
           <p>---Content goes here---</p>
         </div>
