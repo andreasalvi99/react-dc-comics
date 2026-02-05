47f5a2a3b7356cc057464f5c724ebdc9dba15867
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-05 09:52:05 +0100

    changed imports order

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
-import Footer from "./components/Footer";
 import Main2 from "./components/Main2";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
