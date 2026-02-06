7a2052f3afec88412f87dba4c5a7a613fb5fc439
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-06 14:16:37 +0100

    props init

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,11 @@
-import comics from "../../comics";
+import Comics from "./props/Comics";
+
+Comics;
 
 export default function Main() {
   return (
     <main>
-      <section id="contents" className="bg-black">
-        <div id="jumbotron">
-          <button>CURRENT SERIES</button>
-        </div>
-        <div className="container text-light text-center py-5" id="comics">
-          <div className="row row-cols-6 g-4">
-            {comics.map((comic) => (
-              <div key={comic.id} className="col">
-                <figure className="wrapper">
-                  <img src={comic.thumb} alt={comic.title} />
-                </figure>
-                <p className="fs-6 fw-medium text-white opacity-75 py-2 text-start">
-                  {comic.series.toUpperCase()}
-                </p>
-              </div>
-            ))}
-          </div>
-          <button className="mt-5">LOAD MORE</button>
-        </div>
-      </section>
+      <Comics />
     </main>
   );
 }
