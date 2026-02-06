58e59a541d41d3e121da4049fa472cbc3a9d75e7
andreasalvi99 <andrea.salvi.r@gmail.comm>
2026-02-06 15:28:30 +0100

    header props done

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,63 @@ import Main from "./components/Main";
 import Banner from "./components/Banner";
 import Footer from "./components/Footer";
 
+const navbarLinks = [
+  {
+    title: "CHARACTERS",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "COMICS",
+    url: "#",
+    isActive: true,
+  },
+  {
+    title: "MOVIES",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "TV",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "GAMES",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "COLLECTIBLES",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "VIDEOS",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "FANS",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "NEWS",
+    url: "#",
+    isActive: false,
+  },
+  {
+    title: "SHOP",
+    url: "#",
+    isActive: false,
+  },
+];
+
 export default function App() {
   return (
     <>
-      <Header />
+      <Header links={navbarLinks} />
       <Main />
       <Banner />
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,4 @@
-const navbarLinks = [
-  {
-    title: "CHARACTERS",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "COMICS",
-    url: "#",
-    isActive: true,
-  },
-  {
-    title: "MOVIES",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "TV",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "GAMES",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "COLLECTIBLES",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "VIDEOS",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "FANS",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "NEWS",
-    url: "#",
-    isActive: false,
-  },
-  {
-    title: "SHOP",
-    url: "#",
-    isActive: false,
-  },
-];
-
-export default function Header() {
+export default function Header({ links }) {
   return (
     <header>
       <div id="dc-logo" className="py-3">
@@ -59,7 +6,7 @@ export default function Header() {
       </div>
       <div id="navbar" className="">
         <ul>
-          {navbarLinks.map((link, index) => (
+          {links.map((link, index) => (
             <li
               key={index}
               className={"" + (link.isActive ? " active-bar" : "")}
